82eba8c7f7da90aad5fe7a108977a8e31584767c
HenriqueFerreira <henrique.ferreira@ads.uniceplac.edu.br>
2024-04-28 17:07:24 -0300

    atualização de senha e email

--- Login/Login.js
+++ Login/Login.js
@@ -1,3 +1,28 @@
+// const formulario = document.getElementById("formulario");
+
+// formulario.addEventListener("submit", (e) => {
+//   e.preventDefault();
+
+//   logar();
+// });
+
+// function logar() {
+//   const email = document.getElementById("email");
+//   const senha = document.getElementById("senha");
+
+//   const emailDigitado = email.value;
+//   const senhaDigitada = senha.value;
+
+//   if (emailDigitado === "" && senhaDigitada === "") {
+//     window.location.href = "../Dashboard/dashboard.html";
+//   } else {
+//     alert("Usuario ou senha estão incorreto, tente novamente!");
+//   }
+// }
+
+// const botao = document.getElementById("entrar");
+// botao.addEventListener("click", logar);
+
 const formulario = document.getElementById("formulario");
 
 formulario.addEventListener("submit", (e) => {
@@ -13,11 +38,12 @@ function logar() {
   const emailDigitado = email.value;
   const senhaDigitada = senha.value;
 
-  if (emailDigitado === "" && senhaDigitada === "") {
-    window.location.href = "../Dashboard/dashboard.html";
-  } else {
-    alert("Usuario ou senha estão incorreto, tente novamente!");
-  }
+  // Remova a condição que verifica se os campos estão vazios
+  // if (emailDigitado === "" && senhaDigitada === "") {
+  window.location.href = "../Dashboard/dashboard.html";
+  // } else {
+  //   alert("Usuario ou senha estão incorreto, tente novamente!");
+  // }
 }
 
 const botao = document.getElementById("entrar");
